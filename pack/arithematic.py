f24ea4ddcec1850f2352af137ddff882cc3da012
unknown <19kq1a0523@pace.ac.in>
2021-01-08 15:49:18 +0530

    myfourthcommit

---
+++ pack/arithematic.py
@@ -0,0 +1,17 @@
+def add(*a):
+    s = 0
+    for i in a:
+        s+=i
+    return s
+def sub(a,b):
+    s = a-b
+    return s
+def mul(a,b):
+    s = a*b
+    return s
+def div(a,b):
+    s = a/b
+    return s
+def floordiv(a,b):
+    s = a//b
+    return s    
---
+++ pack/sample.py
@@ -0,0 +1,6 @@
+"'welcome to Python World'"
+def sample():
+    print('welcome to Module')
+sample()
+def add(*a):
+    return sum(a)
